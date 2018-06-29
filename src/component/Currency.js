7f66222886ef5982dde306ad3c5394e4b1ed70f3
Daniel Show <danielshotonwa53@gmail.com>
2018-06-29 09:21:04 +0100

    Added Container to currency input

--- src/component/Currency.js
+++ src/component/Currency.js
@@ -19,28 +19,30 @@ class Currency extends Component{
 
   render(){
     return(
-      <div className = "row">
-          <div className = "col-md-8">
-            <h3 className="text-center">Currency Converter</h3>
-            <input type='text' className = "form-control form-control-sm" value={this.state.convertfrom} onChange = {this.handleChange}></input>
-            <select id="CURR_FROM" className="mb-4">
-                <option value="BYN" >BYN</option>
-                <option value="GBP" selected>GBP</option>
-                <option value="USD">USD</option>
-                <option value="UYU" >UYU</option>
-            </select>
-            <input type='text' className = "form-control form-control-sm" value={this.state.convertto}></input>
-            <select id="CURR_TO" className= "mb-4">
-                <option value="BYN" >BYN</option>
-                <option value="GBP">GBP</option>
-                <option value="USD selected">USD</option>
-                <option value="UYU" >UYU</option>
-            </select>
-          </div>
-              my portfolio
-          <div className= "col-md-4">
+      <div className="container">
+        <div className = "row">
+            <div className = "col-md-8">
+              <h3 className="text-center">Currency Converter</h3>
+              <input type='text' className = "form-control form-control-sm" value={this.state.convertfrom} onChange = {this.handleChange}></input>
+              <select id="CURR_FROM" className="mb-4 mt-2">
+                  <option value="BYN" >BYN</option>
+                  <option value="GBP" selected>GBP</option>
+                  <option value="USD">USD</option>
+                  <option value="UYU" >UYU</option>
+              </select>
+              <input type='text' className = "form-control form-control-sm" value={this.state.convertto}></input>
+              <select id="CURR_TO" className= "mb-4 mt-2">
+                  <option value="BYN" >BYN</option>
+                  <option value="GBP">GBP</option>
+                  <option value="USD selected">USD</option>
+                  <option value="UYU" >UYU</option>
+              </select>
+            </div>
+                my portfolio
+            <div className= "col-md-4">
 
-          </div>
+            </div>
+        </div>
       </div>
     );
   }
